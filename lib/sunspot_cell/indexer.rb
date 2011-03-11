e8a6348888212f645af4491086e56ac69b7b2288
Etienne van Tonder <etiennevt@gmail.com>
2011-03-11 18:10:45 +1100

    Fix raise

--- lib/sunspot_cell/indexer.rb
+++ lib/sunspot_cell/indexer.rb
@@ -25,7 +25,7 @@ module SunspotCell
               end
             rescue Exception => e
               @batch = nil
-              raise
+              raise e
             end
           end
         end
